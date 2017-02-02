d593c653b728b97cbc4d71c74b990a414f53b143
cheesenonion89 <m.olig03@googlemail.com>
2017-02-02 18:49:37 +0100

    basic bar plot

--- plot.py
+++ plot.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import plotly.graph_objs as go
+import plotly.plotly as py
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+# plotly.tools.set_credentials_file(username='molig', api_key='InQv6RKZ00uHRwx5lMKJ')
 
 engine_logs = pd.read_csv('df_engine_logs.csv', header=0, sep=';')
 bot_logs = pd.read_csv('df_bot_logs.csv', header=0, sep=';')
@@ -6,6 +11,10 @@ bot_logs = pd.read_csv('df_bot_logs.csv', header=0, sep=';')
 
 def get_engine_games_by_player(player1, player2):
     return engine_logs.loc[(engine_logs['player1'] == player1) & (engine_logs['player2'] == player2)]
+
+
+def get_bot_log_by_bot_type(bot_type):
+    return bot_logs[(bot_logs['bot_type'] == bot_type)]
 
 
 def get_winning_stats(player1, player2):
@@ -22,5 +31,36 @@ def get_player_combinations(df):
     return df[['player1', 'player2']].drop_duplicates()
 
 
-print(get_winning_stats('CONTEST', 'FINAL1'))
-print(get_winning_stats('BASIC', 'CONTEST'))
+def get_depth_traces_by_game(bot_type, game_number):
+    df = get_bot_log_by_bot_type(bot_type)
+    df = df[(df['game_number'] == game_number)][['depth']]
+    return df['depth'].tolist()
+
+
+def get_depth_traces_by_round(bot_type, round_number):
+    df = get_bot_log_by_bot_type(bot_type)
+    df = df[(df['round_number'] == round_number)][['depth']]
+    return df['depth'].tolist()
+
+
+print(get_depth_traces_by_round('final1', 1))
+layout = go.Layout(title='Depth per Round - Final 1', width=1024, height=800)
+data = []
+for index in range(0, 100):
+    data.append(go.Box(
+        name=str(index),
+        y=get_depth_traces_by_round('final1', index)
+    ))
+
+plot = go.Figure(data=data, layout=layout)
+
+py.image.save_as(plot, filename='bars.png')
+
+
+
+
+# print(get_winning_stats('CONTEST', 'FINAL1'))
+# print(get_winning_stats('BASIC', 'CONTEST'))
+
+# print(get_bot_log_by_bot_type('final1')['player1'].count())
+# print(get_bot_log_by_bot_type('final1').groupby(['player1','player2'])['bot_type'].count())
