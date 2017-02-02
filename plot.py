a4be3290f16b8da54a9ae4e5557bd99b4ae8d741
cheesenonion89 <m.olig03@googlemail.com>
2017-02-02 19:08:38 +0100

    depth by round number per bot

--- plot.py
+++ plot.py
@@ -3,18 +3,25 @@ import plotly.graph_objs as go
 import plotly.plotly as py
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 
-# plotly.tools.set_credentials_file(username='molig', api_key='InQv6RKZ00uHRwx5lMKJ')
 
 engine_logs = pd.read_csv('df_engine_logs.csv', header=0, sep=';')
 bot_logs = pd.read_csv('df_bot_logs.csv', header=0, sep=';')
 
+# Utils
+pretty_names = {"basicNegamax": "Basic NegaMax",
+         "contestNegamax": "Contest NegaMax",
+         "final1": "Final 1",
+         "final2": "Final 2"
+         }
 
+
+def get_player_combinations(df):
+    return df[['player1', 'player2']].drop_duplicates()
+
+
+# Functions on engine_logs
 def get_engine_games_by_player(player1, player2):
     return engine_logs.loc[(engine_logs['player1'] == player1) & (engine_logs['player2'] == player2)]
-
-
-def get_bot_log_by_bot_type(bot_type):
-    return bot_logs[(bot_logs['bot_type'] == bot_type)]
 
 
 def get_winning_stats(player1, player2):
@@ -27,8 +34,13 @@ def get_winning_stats(player1, player2):
     return result
 
 
-def get_player_combinations(df):
-    return df[['player1', 'player2']].drop_duplicates()
+# Functions on bot_logs
+def get_bot_types():
+    return bot_logs['bot_type'].drop_duplicates().tolist()
+
+
+def get_bot_log_by_bot_type(bot_type):
+    return bot_logs[(bot_logs['bot_type'] == bot_type)]
 
 
 def get_depth_traces_by_game(bot_type, game_number):
@@ -43,18 +55,19 @@ def get_depth_traces_by_round(bot_type, round_number):
     return df['depth'].tolist()
 
 
-print(get_depth_traces_by_round('final1', 1))
-layout = go.Layout(title='Depth per Round - Final 1', width=1024, height=800)
-data = []
-for index in range(0, 100):
-    data.append(go.Box(
-        name=str(index),
-        y=get_depth_traces_by_round('final1', index)
-    ))
+# Plotting
+for bot in get_bot_types():
+    layout = go.Layout(title='Depth per Round - ' + pretty_names[bot], width=1024, height=800)
+    data = []
+    for index in range(0, 100):
+        data.append(go.Box(
+            name=str(index),
+            y=get_depth_traces_by_round(bot, index)
+        ))
+    plot = go.Figure(data=data, layout=layout)
+    py.image.save_as(plot, filename='depth_by_round/depth_by_round_' + bot + '.png')
 
-plot = go.Figure(data=data, layout=layout)
 
-py.image.save_as(plot, filename='bars.png')
 
 
 
